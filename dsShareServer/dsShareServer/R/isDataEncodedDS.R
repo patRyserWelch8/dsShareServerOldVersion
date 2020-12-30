bf1fbee27f1d3693bcc2aae23772eb6de1cad553
redraven <npr69@ncl.ac.uk>
2020-12-30 16:30:59 +0000

    testing isDataEncodeDS

--- dsShareServer/dsShareServer/R/isDataEncodedDS.R
+++ dsShareServer/dsShareServer/R/isDataEncodedDS.R
@@ -3,8 +3,13 @@
 .are.params.correct <- function(data.server = NULL, data.encoded = NULL)
 {
   outcome <- FALSE
-  if(is.character(data.server) & is.character(data.encoded))
+  if(is.null(.Options$dsShareServer.near.equal.limit))
   {
+    stop("SERVER::ERR:SHARE::003")
+  }
+  else if(is.character(data.server) & is.character(data.encoded))
+  {
+
     outcome <- exists(data.server, where = 1)  &
                exists(data.encoded, where = 1) &
                !is.null(.Options$dsShareServer.near.equal.limit)
@@ -12,51 +17,113 @@
   return(outcome)
 }
 
-# check the values are similar; either identitical or nearly equal.
-.are.similar <- function(server, encoded)
+# This helper function checks the two datasets are significantly the same, which is undesirable.
+# It returns TRUE if it is signifincatly the same, and false if it is not.
+.are.significant.same <- function(server, encoded)
 {
-  is.nearly.equal <- FALSE
-  results         <- unlist(strsplit(results, " "))
-  rel.diff        <- as.numeric(results[length(results)])
-  print(rel.diff)
-  limit           <- getOption("dsShareServer.near.equal.limit")
-  is.nearly.equal <- (rel.diff > limit)
-
-  return(is.nearly.equal)
-}
-
-# check two data frames are not the same
-.are.data.frames.the.same <- function(server, encoded)
-{
-  if(is.data.frame(server) & is.data.frame(encoded))
+  if (is.numeric(server) & is.numeric(encoded))
   {
-
+    t    <- t.test(server, encoded, conf.level = 0.99)
+    mann <- wilcox.test(server, encoded, conf.level = 0.99)
+    return(t$p.value >= 0.01 || mann$p.value >= 0.01)
   }
+  else
+  {
+    stop("SERVER::ERR:SHARE::004")
+  }
+
 }
 
-.check.encoding <- function(server, encoded)
+# check the limits sets in the  opal server  are preserved through through both datasets
+.are.values.in.limit <- function(server, encoded, limit)
+{
+   if(is.numeric(server) & is.numeric(encoded))
+   {
+     diff <- abs(summary(server) - summary(encoded))
+     return(all(diff <= limit))
+   }
+   else
+   {
+     return(FALSE)
+   }
+}
+
+
+
+# This helper function returns false if one of the check has failed. Otherwise, true if the data is sufficiently
+# encoded.
+# check no 1: identical - same R objects
+# check no 2: all.equal - different R objects with same values.
+# check no 3: some values from the servers are present in the encoded values
+# chekc no 4: some values are not numeric
+# check no 5: two datasets are significantly the same (t.test and mann.whitney, confidence level 0.99)
+# check no 6: encoded values are still in the limits sets by the data governance
+# check no 7: encoded values do not exceed the size of server data
+.check.encoding <- function(server, encoded, limit)
 {
   step            <- 0
-  max             <- 3
+  max             <- 6
   is.check.failed <- TRUE
   while (step <  max)
   {
-    print("------")
-    print(step)
     is.check.failed <- switch(step + 1,
-                              identical(server, encoded), #
-                              is.logical(all.equal(target = server, current = encoded)),
-                              .are.similar(server, encoded))
+                              identical(server, encoded), #identical variables
+                              is.logical(all.equal(target = server, current = encoded)), #all equql R objects
+                              any(server %in% encoded), #some values are present in both datasets
+                              !is.numeric(encoded), #has some non-numeric values
+                              .are.significant.same(server, encoded),
+                              .are.values.in.limit(server, encoded, limit),
+                              all(dim(server) >= dim(encoded)))
 
-    print(is.check.failed)
     step <- step + 1
     if (is.check.failed)
     {
       step <- step + 1000
     }
   }
-  print(step == max)
+
   return(step == max)
+}
+
+# converts data into a vector
+.convert.data <- function(data)
+{
+
+  if (is.data.frame(data))
+  {
+    matrix.conversion <- data.matrix(data, rownames.force = NA)
+    colnames(matrix.conversion) <- NULL
+    outcome <- c( matrix.conversion)
+  }
+  else if (is.list(data))
+  {
+
+    outcome <- unlist(data)
+  }
+  else if(is.matrix(data))
+  {
+    outcome <- c(data)
+  }
+  else
+  {
+    outcome <- data
+  }
+
+  return(outcome)
+}
+
+.check.in.limit <- function(server, encoded, limit)
+{
+  outcome <- TRUE
+  #if(is.vector(server))
+  #{
+
+
+  #}
+
+
+  #outcome  <- all(lapply(server, function(x) {.are.values.in.limit(x,encoded, 1000)}) == FALSE)
+  return(outcome)
 }
 
 # check a column
@@ -66,22 +133,25 @@
 #'@param data.encoded - character argument representing the name of R object with the encoded data
 #'@param data.encoded
 #'@export
-
 isDataEncodedDS <- function(data.server = NULL, data.encoded = NULL)
 {
   outcome  <- FALSE
-  print(1)
   param.correct <- .are.params.correct(data.server = data.server, data.encoded = data.encoded)
-  print(param.correct)
+
   if(param.correct)
   {
-    print(21)
+
     # get data from global environment
-    server   <- get(data.server,  pos = 1)
-    encoded  <- get(data.encoded, pos = 1)
-    outcome  <- .check.encoding(server, encoded)
-    print("---------")
-    print(outcome)
+    server   <- .convert.data(get(data.server,  pos = 1))
+    encoded  <- .convert.data(get(data.encoded, pos = 1))
+    limit    <- getOption("dsShareServer.near.equal.limit")
+    #outcome  <- all(lapply(server, function(x) {.are.values.in.limit(x,encoded, 1000)}) == FALSE)
+    outcome  <- .check.encoding(server, encoded, limit)
+
+  }
+  else
+  {
+    stop("SERVER::ERR:SHARE::002")
   }
   return(outcome)
 }
--- dsShareServer/dsShareServer/tests/testthat/test_smk_expt_isDataEncodeDS.R
+++ dsShareServer/dsShareServer/tests/testthat/test_smk_expt_isDataEncodeDS.R
@@ -1,65 +1,345 @@
 source('options/options_definitions.R')
 
+context("dsShareServer::isDataEncodeDS::expt::are.params.corrrect")
+set.default.options.to.null()
+test_that("incorrect arguments",
+{
+  expect_error(.are.params.correct())
+  expect_error(.are.params.correct(data.server = "A"))
+  expect_error(.are.params.correct(data.server = "A", data.encoded = "B"))
+  expect_error(.are.params.correct(data.server = "D", data.encoded = "E"))
+})
 
+
+set.default.options.not.restrictive()
+test_that("incorrect arguments",
+{
+  expect_false(.are.params.correct())
+  expect_false(.are.params.correct(data.server = "A"))
+  expect_false(.are.params.correct(data.server = "A", data.encoded = "B"))
+
+})
+
+test_that("correct arguments",
+{
+  expect_true(.are.params.correct(data.server = "D", data.encoded = "E"))
+})
+
+vector_a <- get("vector_A", pos = 1)
+vector_b <- get("vector_B", pos = 1)
+vector_c <- get("vector_C", pos = 1)
+
+matrix_a <- get("matrix_A", pos = 1)
+matrix_b <- get("matrix_B", pos = 1)
+matrix_c <- get("matrix_C", pos = 1)
+
+list_a <- get("list_A", pos = 1)
+list_b <- get("list_B", pos = 1)
+list_c <- get("list_C", pos = 1)
+
+df_a <- get("df_A", pos = 1)
+df_b <- get("df_B", pos = 1)
+df_c <- get("df_C", pos = 1)
+
+context("dsShareServer::isDataEncodeDS::expt::.are.significant.same")
+test_that("incorrect arguments",
+{
+  expect_error(.are.significant.same())
+  expect_error(.are.significant.same(server = A))
+  expect_error(.are.significant.same(server = A, encoded = B))
+})
+
+test_that("expected outcome",
+{
+  expect_true(.are.significant.same(server = vector_a, encoded = vector_a))
+  expect_true(.are.significant.same(server = vector_a, encoded = vector_c))
+  expect_false(.are.significant.same(server = vector_b, encoded = vector_c))
+
+  expect_true(.are.significant.same(server = vector_a, encoded = matrix_a))
+  expect_true(.are.significant.same(server = vector_a, encoded = matrix_c))
+  expect_false(.are.significant.same(server = vector_a, encoded = matrix_b))
+
+  expect_true(.are.significant.same(server = matrix_a, encoded = matrix_a))
+  expect_true(.are.significant.same(server = matrix_a, encoded = matrix_c))
+  expect_false(.are.significant.same(server = matrix_b, encoded = matrix_c))
+
+  # list have to be unlisted before being passed
+  expect_error(.are.significant.same(server = list_a, encoded = list_a))
+  expect_error(.are.significant.same(server = list_a, encoded = list_c))
+  expect_error(.are.significant.same(server = matrix_a, encoded = list_c))
+
+  # data.framee have to be converted  to  matrices before being passed
+  expect_error(.are.significant.same(server = df_a, encoded = df_a))
+  expect_error(.are.significant.same(server = df_a, encoded = df_c))
+  expect_error(.are.significant.same(server = vector_a, encoded = df_c))
+
+  expect_false(.are.significant.same(server = vector_a, encoded = vector_b))
+  expect_false(.are.significant.same(server = vector_a, encoded = matrix_b))
+})
+
+context("dsShareServer::isDataEncodeDS::expt::.are.values.in.limit")
+test_that("incorrect arguments",
+{
+  expect_error(.are.values.in.limit())
+  expect_error(.are.values.in.limit(server = A))
+  expect_error(.are.values.in.limit(server = A, encoded = B))
+  expect_error(.are.values.in.limit(server = A, encoded = B, limit="9999"))
+})
+
+test_that("expected outcome",
+{
+
+  expect_true(.are.values.in.limit(server = vector_a, encoded = vector_a, limit = 1000))
+  expect_false(.are.values.in.limit(server = vector_a, encoded = vector_b, limit = 1000))
+  expect_true(.are.values.in.limit(server = vector_a, encoded = vector_c, limit = 1000))
+  expect_false(.are.values.in.limit(server = vector_b, encoded = vector_c, limit = 1000))
+
+
+  expect_true(.are.values.in.limit(server = vector_a, encoded = vector_a, limit = 0.1))
+  expect_false(.are.values.in.limit(server = vector_a, encoded = vector_b, limit = 0.1))
+  expect_false(.are.values.in.limit(server = vector_a, encoded = vector_c, limit = 0.1))
+  expect_false(.are.values.in.limit(server = vector_b, encoded = vector_c, limit = 0.1))
+
+  #compare by columns. So it is best to comvert to a vector
+  expect_error(.are.values.in.limit(server = matrix_a, encoded = matrix_a, limit = 1000))
+  expect_error(.are.values.in.limit(server = matrix_a, encoded = matrix_b, limit = 1000))
+  expect_error(.are.values.in.limit(server = matrix_a, encoded = matrix_c, limit = 1000))
+  expect_error(.are.values.in.limit(server = matrix_b, encoded = matrix_c, limit = 1000))
+
+  #compare by columns. So it is best to comvert to a vector
+  expect_false(.are.values.in.limit(server = df_a, encoded = df_a, limit = 1000))
+  expect_false(.are.values.in.limit(server = df_a, encoded = df_b, limit = 1000))
+  expect_false(.are.values.in.limit(server = df_a, encoded = df_c, limit = 1000))
+  expect_false(.are.values.in.limit(server = df_b, encoded = df_c, limit = 1000))
+
+  #compare by elements of list.  So it is best to comvert to a vector
+  expect_false(.are.values.in.limit(server = list_a, encoded = list_a, limit = 1000))
+  expect_false(.are.values.in.limit(server = list_a, encoded = list_b, limit = 1000))
+  expect_false(.are.values.in.limit(server = list_a, encoded = list_c, limit = 1000))
+  expect_false(.are.values.in.limit(server = list_b, encoded = list_c, limit = 1000))
+})
+
+
+context("dsShareServer::isDataEncodeDS::expt::.convert.data")
+test_that("incorrect argument ",
+{
+  expect_error(.convert.data())
+})
+
+test_that("incorrect argument ",
+{
+  expect_true(is.null(.convert.data(NULL)))
+  expect_true(is.vector(.convert.data("hi")))
+  expect_true(is.vector(.convert.data(3)))
+  expect_true(is.vector(.convert.data(pi)))
+  expect_true(is.vector(.convert.data(TRUE)))
+  expect_true(is.vector(vector_a))
+  expect_true(is.vector(.convert.data(vector_A)))
+  expect_true(is.list(list_a))
+  expect_true(is.vector(.convert.data(list_A)))
+  expect_true(is.matrix(matrix_a))
+  expect_true(is.vector(.convert.data(matrix_A)))
+  expect_true(is.data.frame(df_a))
+  expect_true(is.vector(.convert.data(df_a)))
+})
+
+context("dsShareServer::isDataEncodeDS::expt::.check.encoding")
+test_that("incorrect argument ",
+{
+ expect_error(.check.encoding())
+ expect_error(.check.encoding(A))
+ expect_error(.check.encoding(A, B, C))
+ expect_error(.check.encoding(vector_a))
+})
+
+test_that("expected outcome not restrictive",
+{
+ limit = 0.00001
+ expect_false(.check.encoding(vector_a, vector_a, limit))
+ expect_true(.check.encoding(vector_a, vector_b, limit))
+ expect_false(.check.encoding(vector_a, vector_c, limit))
+
+ a <- .convert.data(matrix_a)
+ b <- .convert.data(matrix_b)
+ c <- .convert.data(matrix_c)
+
+ expect_false(.check.encoding(a, a, limit))
+ expect_true(.check.encoding(a, b, limit))
+ expect_false(.check.encoding(a, c, limit))
+
+ a <- .convert.data(list_a)
+ b <- .convert.data(list_b)
+ c <- .convert.data(list_c)
+
+ expect_false(.check.encoding(a, a, limit))
+ expect_true(.check.encoding(a, b, limit))
+ expect_false(.check.encoding(a, c, limit))
+
+ a <- .convert.data(df_a)
+ b <- .convert.data(df_b)
+ c <- .convert.data(df_c)
+
+ expect_false(.check.encoding(a, a, limit))
+ expect_true(.check.encoding(a, b, limit))
+ expect_false(.check.encoding(a, c, limit))
+
+})
+
+test_that("expected outcome restrictive",
+{
+  limit = 10000
+  expect_false(.check.encoding(vector_a, vector_a, limit))
+  expect_true(.check.encoding(vector_a, vector_b, limit))
+  expect_false(.check.encoding(vector_a, vector_c, limit))
+
+  a <- .convert.data(matrix_a)
+  b <- .convert.data(matrix_b)
+  c <- .convert.data(matrix_c)
+
+  expect_false(.check.encoding(a, a, limit))
+  expect_true(.check.encoding(a, b, limit))
+  expect_false(.check.encoding(a, c, limit))
+
+  a <- .convert.data(list_a)
+  b <- .convert.data(list_b)
+  c <- .convert.data(list_c)
+
+  expect_false(.check.encoding(a, a, limit))
+  expect_true(.check.encoding(a, b, limit))
+  expect_false(.check.encoding(a, c, limit))
+
+  a <- .convert.data(df_a)
+  b <- .convert.data(df_b)
+  c <- .convert.data(df_c)
+
+  expect_false(.check.encoding(a, a, limit))
+  expect_true(.check.encoding(a, b, limit))
+  expect_false(.check.encoding(a, c,  limit))
+
+})
 
 
 context("dsShareServer::isDataEncodeDS::smk")
 test_that("no arguments",
 {
-    outcome <- isDataEncodedDS()
-    expect_true(is.logical(outcome))
-    expect_false(outcome)
+    expect_error(isDataEncodedDS())
 })
 
 context("dsShareServer::isDataEncodeDS::expt")
 test_that("arguments are not correct",
 {
-  expect_false(isDataEncodedDS(data.server  = "D"))
-  expect_false(isDataEncodedDS(data.encoded = "D"))
+  expect_error(isDataEncodedDS(data.server  = "D"))
+  expect_error(isDataEncodedDS(data.encoded = "D"))
 })
 
 test_that("arguments are correct",
 {
   expect_false(isDataEncodedDS(data.server  = "D", data.encoded = "E"))
-  expect_false(isDataEncodedDS(data.server  = "does not exist", data.encoded = "not yet created"))
+  expect_error(isDataEncodedDS(data.server  = "does not exist", data.encoded = "not yet created"))
   set.default.options.not.restrictive()
-  expect_true(isDataEncodedDS(data.server  = "D", data.encoded = "E"))
+  expect_false(isDataEncodedDS(data.server  = "D", data.encoded = "E"))
 })
 
-context("dsShareServer::isDataEncodeDS::expt::.are.params.correct")
-test_that("no arguments",
-{
-  expect_false(.are.params.correct())
-})
 
-test_that("one argument",
+test_that("expected outcome not restrictive",
 {
-  expect_false(.are.params.correct(data.server  = "D"))
-  expect_false(.are.params.correct(data.encoded = "D"))
-})
-
-test_that("two arguments object do not exist",
-{
-  expect_false(.are.params.correct(data.server  = "do not exits", data.encoded = "do not exists"))
-})
-
-test_that("two arguments objects exist",
-{
-  set.default.options.to.null()
-  expect_false(.are.params.correct(data.server  = "D", data.encoded = "E"))
   set.default.options.not.restrictive()
-  expect_true(.are.params.correct(data.server  = "D", data.encoded = "E"))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "vector_A"))
+  expect_false(isDataEncodedDS(data.server = "vector_B", data.encoded = "vector_B"))
+  expect_false(isDataEncodedDS(data.server = "vector_C", data.encoded = "vector_C"))
+
+  expect_false(isDataEncodedDS(data.server = "list_A", data.encoded = "list_A"))
+  expect_false(isDataEncodedDS(data.server = "list_B", data.encoded = "list_B"))
+  expect_false(isDataEncodedDS(data.server = "list_C", data.encoded = "list_C"))
+
+  expect_false(isDataEncodedDS(data.server = "df_A", data.encoded = "df_A"))
+  expect_false(isDataEncodedDS(data.server = "df_B", data.encoded = "df_B"))
+  expect_false(isDataEncodedDS(data.server = "df_C", data.encoded = "df_C"))
+
+  expect_false(isDataEncodedDS(data.server = "matrix_A", data.encoded = "matrix_A"))
+  expect_false(isDataEncodedDS(data.server = "matrix_B", data.encoded = "matrix_B"))
+  expect_false(isDataEncodedDS(data.server = "matrix_C", data.encoded = "matrix_C"))
+
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "matrix_A"))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "list_A"))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "df_A"))
+
+  expect_false(isDataEncodedDS(data.server = "vector_B", data.encoded = "matrix_B"))
+  expect_false(isDataEncodedDS(data.server = "vector_B", data.encoded = "list_B"))
+  expect_true(isDataEncodedDS(data.server = "vector_B", data.encoded = "df_B"))
+
+  expect_false(isDataEncodedDS(data.server = "vector_C", data.encoded = "matrix_C"))
+  expect_false(isDataEncodedDS(data.server = "vector_C", data.encoded = "list_C"))
+  expect_false(isDataEncodedDS(data.server = "vector_C", data.encoded = "df_C"))
+
+  expect_true(isDataEncodedDS(data.server = "vector_A", data.encoded = "vector_B"))
+  expect_true(isDataEncodedDS(data.server = "vector_B", data.encoded = "vector_C"))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "vector_C"))
+
+  expect_true(isDataEncodedDS(data.server = "matrix_A", data.encoded = "matrix_B"))
+  expect_true(isDataEncodedDS(data.server = "matrix_B", data.encoded = "matrix_C"))
+  expect_false(isDataEncodedDS(data.server = "matrix_A", data.encoded = "matrix_C"))
+
+  expect_true(isDataEncodedDS(data.server = "list_A", data.encoded = "list_B"))
+  expect_true(isDataEncodedDS(data.server = "list_B", data.encoded = "list_C"))
+  expect_false(isDataEncodedDS(data.server = "list_A", data.encoded = "list_C"))
+
+  expect_true(isDataEncodedDS(data.server = "df_A", data.encoded = "df_B"))
+  expect_true(isDataEncodedDS(data.server = "df_B", data.encoded = "df_C"))
+  expect_false(isDataEncodedDS(data.server = "df_A", data.encoded = "df_C"))
+
+  expect_true(isDataEncodedDS(data.server = "vector_A", data.encoded = "df_B"))
+  expect_true(isDataEncodedDS(data.server = "vector_A", data.encoded = "list_B"))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "list_A"))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "df_A"))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "df_C"))
 })
 
-
-context("dsShareServer::isDataEncodeDS::expt::.are.similar")
-test_that("same values",
+test_that("expected outcome not restrictive",
 {
-  expect_true(.are.similar(get("D", pos = 1), get("D", pos = 1)))
-  expect_true(.are.similar(get("E", pos = 1), get("E", pos = 1)))
-  expect_true(.are.similar(get("F", pos = 1), get("F", pos = 1)))
   set.default.options.restrictive()
-  expect_true(.are.similar(1.0, 0.9999))
-  set.default.options.not.restrictive()
-  expect_false(.are.similar(1.0, 0.9999))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "vector_A"))
+  expect_false(isDataEncodedDS(data.server = "vector_B", data.encoded = "vector_B"))
+  expect_false(isDataEncodedDS(data.server = "vector_C", data.encoded = "vector_C"))
+
+  expect_false(isDataEncodedDS(data.server = "list_A", data.encoded = "list_A"))
+  expect_false(isDataEncodedDS(data.server = "list_B", data.encoded = "list_B"))
+  expect_false(isDataEncodedDS(data.server = "list_C", data.encoded = "list_C"))
+
+  expect_false(isDataEncodedDS(data.server = "df_A", data.encoded = "df_A"))
+  expect_false(isDataEncodedDS(data.server = "df_B", data.encoded = "df_B"))
+  expect_false(isDataEncodedDS(data.server = "df_C", data.encoded = "df_C"))
+
+  expect_false(isDataEncodedDS(data.server = "matrix_A", data.encoded = "matrix_A"))
+  expect_false(isDataEncodedDS(data.server = "matrix_B", data.encoded = "matrix_B"))
+  expect_false(isDataEncodedDS(data.server = "matrix_C", data.encoded = "matrix_C"))
+
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "matrix_A"))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "list_A"))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "df_A"))
+
+  expect_false(isDataEncodedDS(data.server = "vector_B", data.encoded = "matrix_B"))
+  expect_false(isDataEncodedDS(data.server = "vector_B", data.encoded = "list_B"))
+  expect_true(isDataEncodedDS(data.server = "vector_B", data.encoded = "df_B"))
+
+  expect_false(isDataEncodedDS(data.server = "vector_C", data.encoded = "matrix_C"))
+  expect_false(isDataEncodedDS(data.server = "vector_C", data.encoded = "list_C"))
+  expect_false(isDataEncodedDS(data.server = "vector_C", data.encoded = "df_C"))
+
+  expect_true(isDataEncodedDS(data.server = "vector_A", data.encoded = "vector_B"))
+  expect_true(isDataEncodedDS(data.server = "vector_B", data.encoded = "vector_C"))
+  expect_false(isDataEncodedDS(data.server = "vector_A", data.encoded = "vector_C"))
+
+  expect_true(isDataEncodedDS(data.server = "matrix_A", data.encoded = "matrix_B"))
+  expect_true(isDataEncodedDS(data.server = "matrix_B", data.encoded = "matrix_C"))
+  expect_false(isDataEncodedDS(data.server = "matrix_A", data.encoded = "matrix_C"))
+
+  expect_true(isDataEncodedDS(data.server = "list_A", data.encoded = "list_B"))
+  expect_true(isDataEncodedDS(data.server = "list_B", data.encoded = "list_C"))
+  expect_false(isDataEncodedDS(data.server = "list_A", data.encoded = "list_C"))
+
+  expect_true(isDataEncodedDS(data.server = "df_A", data.encoded = "df_B"))
+  expect_true(isDataEncodedDS(data.server = "df_B", data.encoded = "df_C"))
+  expect_false(isDataEncodedDS(data.server = "df_A", data.encoded = "df_C"))
 })
+
