59c9b428502b4d51f5334fdb93870f1136041079
redraven <npr69@ncl.ac.uk>
2021-01-27 16:05:51 +0000

    Update assignParamSettingsDS.R

--- R/assignParamSettingsDS.R
+++ R/assignParamSettingsDS.R
@@ -18,7 +18,7 @@
 
   while(!stop)
   {
-     outcome   <- as.vector(runif(no.elements, min = 0.01, max = 0.99))
+     outcome   <- as.vector(stats::runif(no.elements, min = 0.01, max = 0.99))
      values    <- ceiling(outcome * coordinate)
      no.levels <- length(levels(factor(values)))
      stop <- (no.levels == no.elements)
