2cd16488de899c6a98e5c6a4d1f9eead1072e3b6
redraven <npr69@ncl.ac.uk>
2021-01-27 15:44:59 +0000

    documentation error

--- R/package_tools.R
+++ R/package_tools.R
@@ -140,8 +140,8 @@ get.transfer <- function(settings)
 #'@note Throws the following errors:
 #'"SERVER::ERR:SHARE::002"  sharing is not allowed or the disclosure setting has not been set to 0 or 1
 #'"SERVER::ERR:SHARE::005"  data.encoded does not exists on the server
-#'"SERVER::ERR:SHARE::008"  data.encoded is not the same R object as previously validated \source{isDataEncodedDS}
-#'"SERVER::ERR:SHARE::009"  data.encoded has yet to be validated by \source{isDataEncodedDS}
+#'"SERVER::ERR:SHARE::008"  data.encoded is not the same R object as previously validated \code{isDataEncodedDS}
+#'"SERVER::ERR:SHARE::009"  data.encoded has yet to be validated by \code{isDataEncodedDS}
 #'"SERVER::ERR:SHARE::010"  data.encoded is not a character vector
 #'
 are.arg.and.settings.suitable <- function(data.encoded)
